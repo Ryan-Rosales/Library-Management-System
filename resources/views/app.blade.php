42da238b432cc589df865f9bbe3d59a2ec17ffa9
Ryan-Rosales <ryan.rosales@urios.edu.ph>
2026-03-22 16:33:54 +0800

    white screen fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>{{ config('app.name', 'Libraria') }}</title>
-        <link rel="icon" type="image/png" href="/images/Libraria.png">
-        <link rel="apple-touch-icon" href="/images/Libraria.png">
+        <link rel="icon" type="image/png" href="/images/Libraria.png?v=2">
+        <link rel="shortcut icon" href="/images/Libraria.png?v=2">
+        <link rel="apple-touch-icon" href="/images/Libraria.png?v=2">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
